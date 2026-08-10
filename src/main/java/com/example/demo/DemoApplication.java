e1611dbca60acdd023365239ae3818ef209b0b2e
saikumargudi12Git <saikumargudi12@gmail.com>
2026-08-10 09:53:18 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,30 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-public class HelloController {
+public class DemoApplication {
 
-    @GetMapping("/")
-    public String home() {
-        return """
-Java Spring Boot Application
-
-Successfully Deployed on Amazon EKS
-
-DevOps CI/CD Pipeline:
-GitHub Actions -> Maven -> Docker -> Amazon ECR -> Amazon EKS -> Kubernetes
-
-AWS Services Used:
-- Amazon ECR (Container Registry)
-- Amazon EKS (Kubernetes Cluster)
-
-Optional Services (If Required by the Application):
-- Amazon RDS (Relational Database)
-- Amazon S3 (Object Storage)
-""";
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
     }
+
 }
